f9dbcb973335172dfd2dd92161d9d7340467270d
Rouslan abaev <abaev.rouslan@hotmail.fr>
2022-10-13 16:42:56 +0200

    ajout des boutons comm like dislikes et compteur de likes

--- src/Component/Actus/Actus.js
+++ src/Component/Actus/Actus.js
@@ -1,14 +1,32 @@
 import "./Actus.css"
+import { useReducer } from "react";
+
+function countReducer(state, action) {
+    if(action === "increment") {
+        return state + 1;
+    } else if (action === "decrement") {
+        return state - 1;
+    } else {
+        throw new Error();
+    }
+}
 
 function Actus({ title, description }){
+
+    const [count, updateCount] = useReducer(countReducer, 0);
+
     return(
+
         <li className="Actus">
             <h3>{title}</h3>
             <p>{description}</p>
             <div id="Boutons">
-                <button id="Commentaire"></button>
-                <button id="Like"></button>
-                <button id="Dislike"></button>
+                <button id="Commentaire">#</button>
+                <div id="lesLikes">
+                    <button id="Like" onClick={() => updateCount("increment")}>+1</button>
+                    <p id="likeTotal">{count} Likes</p>
+                    <button id="Dislike" onClick={() => updateCount("decrement")}>-1</button>
+                </div>
             </div>
         </li>
     )
