eea798bf612a01619d356e70fd67bae868444ccd
Rouslan abaev <abaev.rouslan@hotmail.fr>
2022-10-14 12:02:26 +0200

    ajout section commentaire dans chaque post avec like dislike

--- src/Component/Actus/Actus.js
+++ src/Component/Actus/Actus.js
@@ -1,5 +1,8 @@
 import "./Actus.css"
 import { useReducer } from "react";
+import { useState } from "react";
+import Commentaire from "../Commentaire/Commentaire";
+
 
 function countReducer(state, action) {
     if(action === "increment") {
@@ -11,9 +14,39 @@ function countReducer(state, action) {
     }
 }
 
-function Actus({ title, description }){
+function Actus({ title, description}){
 
+    //-----------------------------------------------------------//
+
+    // variable d'etat qui compte les likes
     const [count, updateCount] = useReducer(countReducer, 0);
+
+    //-----------------------------------------------------------//
+
+    const [newTask, setNewTask] = useState("");
+    const [tasksList, setTasksList] = useState([]);
+  
+    /**
+     * L'utilisateur a cliqué sur le bouton Validation
+     * Je modifie mes deux variables d'environnements
+     */
+    const addTask = () => {
+      // Ajout de la tache actuelle à mon tableau
+      tasksList.push(newTask);
+  
+      // Mise à jour de la variable d'environnement tasksList
+      setTasksList(tasksList);
+  
+      // Mise à jour de la variable d'environnement newTask
+      setNewTask("");
+    }
+
+    //L'input a changer de valeur, je met à jour ma variable d'environnement newTask
+    const inputChange = (e) =>{
+        setNewTask(e.target.value);
+    }
+
+    //-----------------------------------------------------------//
 
     return(
 
@@ -21,13 +54,19 @@ function Actus({ title, description }){
             <h3>{title}</h3>
             <p>{description}</p>
             <div id="Boutons">
-                <button id="Commentaire" onClick="">#</button>
-                <div id="lesLikes">
+            <div id="SectionAjout">  
+                <button id="addCom" onClick={addTask}>#</button>
+                <input id="Commentaire" onChange={inputChange} value={newTask} placeholder="Trash talkez"></input>
+            </div>
+                <div id="lesLikesPost">
                     <button id="Like" onClick={() => updateCount("increment")}>👍</button>
                     <p id="likeTotal">{count} ❤️</p>
                     <button id="Dislike" onClick={() => updateCount("decrement")}>👎</button>
                 </div>
             </div>
+            <ul id="colonneCom">
+                {tasksList.map((activeTask, index) => <Commentaire content={activeTask} key={index} />)}
+            </ul>
         </li>
     )
 }
