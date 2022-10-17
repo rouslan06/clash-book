10d50f5e3fbf3846f84eebbb3d435a0cabd6a027
ibmow <kibrand@hotmail.fr>
2022-10-17 02:15:52 +0200

    style

--- src/Pages/Page4/Page4.js
+++ src/Pages/Page4/Page4.js
@@ -7,15 +7,35 @@ import { useEffect, useState } from "react";
 
 function Page4() {
 
-  // Création de la variable d'état qui contient la liste des articles
+  // La variable d'état qui contient la liste des articles
   const [articles, setArticles] = useState([]);
 
-  // Création d'une variable d'état qui contient la liste ("")
+  // La variable d'état qui contient la liste ("")
   const [userPage4, setUserPage4] = useState("");
 
-  // Création de la fonction asynchrone
+  // Création de la fonction asynchrone pour récuperer les articles récents
   async function getArticles() {
-        }
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify ({
+        firstname: this.firstname,
+        lastname: this.lastname,
+        email: this.email,
+        password: this.password
+      }),
+    };
+    let response = await fetch (
+      "https://social-network-api.osc-fr1.scalingo.io/clash-book", options);
+    
+
+    let data = await response.json ();
+
+  }
+
+  
 
 
   const handlePage4Change = (e) => {
@@ -29,7 +49,7 @@ function Page4() {
     getArticles();
   },
     [userPage4]);
-  
+
 
 
   return (
@@ -37,43 +57,58 @@ function Page4() {
       <Menu />
       <div id="Page4">
         <h1>Page de Ibrahima</h1>
-        <ul>
-        <p>pages récentes</p>
-        <button> {"retour accueil"}</button>
+        <ul id="entete">
           <p> Liste des articles :</p>
-    
         </ul>
-        <div className="listearticles">
-        <li> Former Uber security chief found guilty:
 
-            <br />
-            Uber paid two hackers $100,000 in Bitcoin to keep a 2016 data breach quiet, and now a jury has convicted former chief security officer Joe Sullivan on two charges for not reporting the incident to authorities.
+        <ul id="lesarticles">
+
+          <li className="articles">
+            <h3>Communiquer</h3>
+            <p>Les réseaux sociaux sont des outils de promotion pour les entreprises commerciales et les fournisseurs de service.
+              Ils sont aussi des vecteurs de changement, de mobilisation</p>
           </li>
-          <br />
-          <li> Former Uber security chief found guilty:
-            <br />
-            Uber paid two hackers $100,000 in Bitcoin to keep a 2016 data breach quiet, and now a jury has convicted former chief security officer Joe Sullivan on two charges for not reporting the incident to authorities.
-          </li>
-          <br />
-          <li> Google’s partnering with Coinbase to let cloud customers pay in crypto next year
-            :
-            <br />
-            Starting in 2023, Google will partner with Coinbase to power crypto-based transactions for “select” customers in the Web3 space.
 
 
+          <li className="articles">
+            <h3>Désinformation</h3>
+
+            <p>Les réseaux sociaux sont une source d’information faramineuse. Toutefois, la désinformation s’y trouve tout autant. Il est de plus en plus difficile de départager ce qui est vrai de ce qui est faux. De plus, les algorithmes utilisés par les réseaux sociaux présentent souvent une seule facette de l’information, celle qui nous conforte dans nos positions.
+              Pour cette raison, les utilisateurs des réseaux sociaux devraient prendre le temps de s’informer à partir des médias extérieurs qui constituent des sources fiables d’informations.</p>
           </li>
-          <br />
-          <li> Former Uber security chief found guilty:
-            <br />
-            Uber paid two hackers $100,000 in Bitcoin to keep a 2016 data breach quiet, and now a jury has convicted former chief security officer Joe Sullivan on two charges for not reporting the incident to authorities.
+
+          <li className="articles">
+            <h3> Vie professionnelle et privée</h3>
+
+            <p>La barrière entre la vie professionnelle et privée s’amenuise avec les réseaux sociaux.
+              Plus les gens s’y affichent, plus les personnes de leur entourage peuvent tout savoir sur eux.
+              Parmi cet entourage, il y a peut-être un futur employeur, ou encore quelqu’un qui cible ses victimes pour commettre un vol.
+              Il ne faut jamais oublier que ce que l’on met sur le web devient public, malgré les paramètres de confidentialité, malgré les images qui, en principe, disparaissent après quelques minutes.
+              Avec les réseaux sociaux, il n’y a plus aucune barrière entre la vie professionnelle et la vie privée.
+              Il faut donc impérativement se questionner par rapport à ce qu’on alimente de notre « vie virtuelle. »</p>
           </li>
-          <br />
-          <li> Former Uber security chief found guilty:
-            <br />
-            Uber paid two hackers $100,000 in Bitcoin to keep a 2016 data breach quiet, and now a jury has convicted former chief security officer Joe Sullivan on two charges for not reporting the incident to authorities.
+
+          <li className="articles">
+            <h3>Briser la solitude?</h3>
+
+            <p>Mal du siècle, la solitude mine le bien-être de nombreuses personnes.
+              Or, les réseaux sociaux comblent une partie de ce vide relationnel,
+              mais en même temps, lorsqu’on en abuse, ils peuvent l’accentuer..</p>
+
           </li>
-          </div>
-          
+          <li className="articles">
+            <h3> Baisse d'estime de soi</h3>
+            <p> On remarque, de plus en plus, une baisse de l’estime de soi chez les utilisateurs assidus des réseaux sociaux, en particulier chez les adolescentes.
+              En se comparant toujours à des images parfaites, à des gens qui semblent vivre continuellement des choses extraordinaires,
+              il semble de plus en plus difficile pour les jeunes de se forger leur propre estime personnelle.</p>
+          </li>
+          <li className="articles">
+            <h3>Les inconvénients des réseaux sociaux</h3>
+            <p>Mais s’ils apportent de nombreux avantages,
+              les réseaux sociaux, comme Internet, sont capables du meilleur comme du pire.</p>
+          </li>
+        </ul>
+
       </div>
     </div>
   );
