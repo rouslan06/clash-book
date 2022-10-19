4faaafec8ad407d015864ae6abce9c22e719ffb3
Cassandra <cassandra.sw@live.fr>
2022-10-19 11:19:54 +0200

    modification du profil css ok

--- src/Component/Profil/Profil.js
+++ src/Component/Profil/Profil.js
@@ -1,12 +1,12 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import "./Profil.css";
 
-function Profil(){
-    const [user, setUser] = useState({firstname: "", lastname: "", email: ""});
+function Profil() {
+    const [user, setUser] = useState({ firstname: "", lastname: "", email: "" });
 
-    async function getInfo(){
+    async function getInfo() {
         let token = JSON.parse(localStorage.getItem("token"));
-    
+
         const optionsInfo = {
             method: "GET",
             headers: {
@@ -14,49 +14,51 @@ function Profil(){
                 "Authorization": `bearer ${token}`
             }
         }
-    
+
         const responseInfo = await fetch("https://social-network-api.osc-fr1.scalingo.io/clash-book/user", optionsInfo);
-    
+
         const dataInfo = await responseInfo.json();
-    
+
         const firstname = dataInfo.firstname;
         const lastname = dataInfo.lastname;
         const email = dataInfo.email;
 
-        setUser({firstname: firstname, lastname: lastname, email: email})
+        setUser({ firstname: firstname, lastname: lastname, email: email })
         console.log(dataInfo);
     }
-    
-    useEffect(()=>{getInfo()},[]);
 
-    function infoChange(e){
+    useEffect(() => { getInfo() }, []);
+
+    function infoChange(e) {
         e.preventDefault();
     }
 
     return (
-        <div className="info">
-            <label htmlFor="firstname">Prénom</label>
-            <input 
-                name="firstname" 
-                value={user.firstname}
-                onChange={infoChange}
-            />
+        <div>
+            <div className="info">
+                <label htmlFor="firstname" id="infoProfilTitle">Prénom :</label>
+                <input id="inputProfil"
+                    name="firstname"
+                    value={user.firstname}
+                    onChange={infoChange}
+                />
 
-            <label htmlFor="lastname">Nom</label>
-            <input 
-                name="lastname" 
-                value={user.lastname}
-                onChange={infoChange}
-            />
+                <label htmlFor="lastname" id="infoProfilTitle">Nom : </label>
+                <input id="inputProfil"
+                    name="lastname"
+                    value={user.lastname}
+                    onChange={infoChange}
+                />
 
-            <label htmlFor="email">Email</label>
-            <input 
-                name="email" 
-                value={user.email}
-                onChange={infoChange}
-            />
+                <label htmlFor="email" id="infoProfilTitle">Email :</label>
+                <input id="inputProfil"
+                    name="email"
+                    value={user.email}
+                    onChange={infoChange}
+                />
 
-            <button>Modifier</button>
+            </div>
+                <button id="profilModifButton">Modifier</button>
         </div>
     )
 }
